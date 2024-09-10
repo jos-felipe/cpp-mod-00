9288e8d84a1826c0d3df0417008f60a681c46b51
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-09-10 15:28:22 -0300

    ex01 ok

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -2,27 +2,27 @@
 #include <iostream>
 
 int main() {
-    PhoneBook phoneBook;
-    std::string command;
+	PhoneBook phoneBook;
+	std::string command;
 
-    while (true) {
-        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-        if (!std::getline(std::cin, command)) {
-            if (std::cin.eof()) 
-                std::cout << "\nEOF detected. Exiting program." << std::endl;
-            break; 
-        }
+	while (true) {
+		std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+		if (!std::getline(std::cin, command)) {
+			if (std::cin.eof()) 
+				std::cout << "\nEOF detected. Exiting program." << std::endl;
+			break; 
+		}
 
-        if (command == "ADD") {
-            phoneBook.addContact();
-        } else if (command == "SEARCH") {
-            phoneBook.searchContact();
-        } else if (command == "EXIT") {
-            break;
-        } else {
-            std::cout << "Invalid command! Please enter ADD, SEARCH, or EXIT." << std::endl;
-        }
-    }
+		if (command == "ADD") {
+			phoneBook.addContact();
+		} else if (command == "SEARCH") {
+			phoneBook.searchContact();
+		} else if (command == "EXIT") {
+			break;
+		} else {
+			std::cout << "Invalid command! Please enter ADD, SEARCH, or EXIT." << std::endl;
+		}
+	}
 
-    return 0;
+	return 0;
 }
