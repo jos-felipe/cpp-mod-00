14a8128fa4969c866177647d9f19050ca13df866
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-09-10 13:37:50 -0300

    phonebook.cpp

--- ex01/include/contact.hpp
+++ ex01/include/contact.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 13:15:55 by josfelip          #+#    #+#             */
+/*   Updated: 2024/09/10 13:17:13 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
@@ -7,23 +19,23 @@
 class Contact
 {
 private:
-    std::string firstName;
-    std::string lastName;
-    std::string nickname;
-    std::string phoneNumber;
-    std::string darkestSecret;
+	std::string firstName;
+	std::string lastName;
+	std::string nickname;
+	std::string phoneNumber;
+	std::string darkestSecret;
 
 
 public:
-    Contact();
-    ~Contact();
+	Contact();
+	~Contact();
 
-    void setContactInfo();
-    void displayContactInfo() const;
-
-    const std::string& getFirstName() const;
-    const std::string& getLastName() const;
-    const std::string& getNickname() const;
+	void setContactInfo();
+	void displayContactInfo() const;
+	
+	const std::string& getFirstName() const;
+	const std::string& getLastName() const;
+	const std::string& getNickname() const;
 };
 
 std::string truncateString(const std::string& str);
--- ex01/include/phonebook.hpp
+++ ex01/include/phonebook.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 13:17:44 by josfelip          #+#    #+#             */
+/*   Updated: 2024/09/10 13:19:59 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
@@ -8,16 +20,15 @@ const int MAX_CONTACTS = 8;
 class PhoneBook
 {
 private:
-    Contact contacts[MAX_CONTACTS];
-    int contactCount;
-    int currentIndex;
+	Contact contacts[MAX_CONTACTS];
+	int contactCount;
+	int currentIndex;
 
 public:
-    PhoneBook();
-    ~PhoneBook();
-
-    void addContact();
-    void searchContact();
+	PhoneBook();
+	~PhoneBook();
+	void addContact();
+	void searchContact();
 };
 
 #endif
