d6c8f94bd806a822982ce12bfa9e92192372997c
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-09-16 16:28:49 -0300

    Code ref has been changed from Bia to Nishimoto

---
+++ ex01/include/Contact.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 13:15:55 by josfelip          #+#    #+#             */
+/*   Updated: 2024/09/16 16:05:25 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+
+class	Contact {
+
+public:
+	Contact(void);
+	Contact(std::string empty);
+	~Contact(void);
+
+	std::string	getFirstName(void) const;
+	std::string	getLastName(void) const;
+	std::string	getNickname(void) const;
+	std::string	getPhoneNumber(void) const;
+	std::string	getDarkestSecret(void) const;
+	void		displayInfo(void) const;
+	
+	bool	setFirstName(std::string input);
+	bool	setLastName(std::string input);
+	bool	setNickname(std::string input);
+	bool	setPhoneNumber(std::string input);
+	bool	setDarkestSecret(std::string input);
+
+private:
+	std::string	_firstName;
+	std::string	_lastName;
+	std::string	_nickname;
+	std::string	_phoneNumber;
+	std::string	_darkestSecret;
+};
+
+#endif
--- ex01/include/Phonebook.hpp
+++ ex01/include/Phonebook.hpp
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 13:17:44 by josfelip          #+#    #+#             */
-/*   Updated: 2024/09/10 13:19:59 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/09/16 15:23:21 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
-#include "contact.hpp"
+#include "Contact.hpp"
 
 const int MAX_CONTACTS = 8;
 
