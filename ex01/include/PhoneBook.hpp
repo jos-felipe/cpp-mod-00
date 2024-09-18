963e031dfd43ed50d4f990b0cfc646d5e4ec6e61
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-09-18 14:30:21 -0300

    PhoneBook.hpp has been reviewed

--- ex01/include/PhoneBook.hpp
+++ ex01/include/PhoneBook.hpp
@@ -1,34 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 13:17:44 by josfelip          #+#    #+#             */
-/*   Updated: 2024/09/16 15:23:21 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/09/18 14:29:17 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
-#define PHONEBOOK_HPP
+# define PHONEBOOK_HPP
 
 #include "Contact.hpp"
 
 const int MAX_CONTACTS = 8;
 
-class PhoneBook
-{
-private:
-	Contact contacts[MAX_CONTACTS];
-	int contactCount;
-	int currentIndex;
+class	PhoneBook {
 
 public:
-	PhoneBook();
-	~PhoneBook();
-	void addContact();
-	void searchContact();
+	PhoneBook(int num);
+	~PhoneBook(void);
+	
+	bool	addContact();
+	void	searchContact(void) const;
+	
+private:
+	Contact	_contacts[MAX_CONTACTS];
+	int		_index;
+
+	std::string	_getInputData(std::string field) const;
+	void		_displayContactList(void) const;
+	std::string	_truncateData(std::string data) const;
 };
 
 #endif
